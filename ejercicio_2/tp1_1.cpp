4b60d9762f1e450aba99a8d0d5b8ed9bf0b01d50
nico018 <nicolas12velasco@gmail.com>
2024-04-08 20:41:18 -0300

    segundo commit del archivo

--- ejercicio_2/tp1_1.cpp
+++ ejercicio_2/tp1_1.cpp
@@ -3,6 +3,19 @@
 int main() {
 
     printf("Hola mundo\n");
+
+    int variable = 10; 
+    int *puntero; 
+
+    puntero = &variable;
+
+    printf("1) Contenido del puntero: %d\n", *puntero);
+    printf("2) Direccion de memoria almacenada por el puntero: %p\n", (void *) puntero);
+    printf("3) Direccinn de memoria de la variable: %p\n", (void *) &variable);
+    printf("4) Direccion de memoria del puntero: %p\n", (void *) &puntero);
+    printf("5) Tamano de memoria utilizado por la variable: %zu bytes\n", sizeof(variable));
+
+    
     return 0;
 }
 
